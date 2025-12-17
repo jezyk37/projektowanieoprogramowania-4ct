ba9b88d9fb1db664037c65412d900a3d6b19bcf9
jezyk <kubusjezyk69@gmail.com>
2025-12-17 08:40:21 +0100

    kartkowka

---
+++ 17.12.2025/Testy1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Zadanie
+{
+    public class Wpisy
+    {
+        static void Main(string[] args)
+        {
+            Wpisy wpisy = new Wpisy();
+            Console.WriteLine("Podaj wpis ==");
+            string tekst = Console.ReadLine();
+            Console.WriteLine(wpisy.sprawdzenie10liter(tekst));
+            Console.WriteLine(wpisy.sprawdzCzyInt(tekst));
+
+            Console.WriteLine("Podaj wpis ==");
+            string tekst2 = Console.ReadLine();
+            Console.WriteLine(wpisy.sprawdzenie10_20liter(tekst2));
+            Console.WriteLine(wpisy.sprawdzCzyString(tekst2)); 
+            
+
+        }
+        public string sprawdzenie10liter(string wpis)
+        {
+            if(wpis.Length == 10)
+            {
+                return "10 liter";
+            }
+            else
+            {
+                return "inna liczba liter";
+            }
+        }
+        public string sprawdzenie10_20liter(string wpis)
+        {
+            if (wpis.Length >= 10 && wpis.Length <= 20)
+            {
+                return "1020 liter";
+            }
+            else
+            {
+                return "nie jest pomiedzy 10-20";
+            }
+        }
+        public string sprawdzCzyString(string wpis)
+        {
+            if (int.TryParse(wpis, out int wynik))
+            {
+                return "inny typ danych";
+            }
+
+            return "string instance";
+        }
+
+        public string sprawdzCzyInt(string wpis)
+        {
+            if (int.TryParse(wpis,out int wynik))
+            {
+                return "int instance";
+            }
+
+            return "inny typ danych";
+        }
+
+
+    }
+}
---
+++ 17.12.2025/Zadanie.Test/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using Zadanie;
+
+namespace Zadanie.Test
+{
+    public class Program
+    {
+        private Wpisy _wpis;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _wpis = new Wpisy();
+        }
+
+        [Test]
+        public void Test_czy_10_liter()
+        {
+            string tekst = "1234567890";
+            string result = "10 liter";
+
+            string act = _wpis.sprawdzenie10liter(tekst);
+
+            Assert.That(act, Is.EqualTo(result));
+        }
+        [Test]
+        public void Test_czy_10_20_liter()
+        {
+            string tekst = "1234567890123";
+            string result = "1020 liter";
+
+            string act = _wpis.sprawdzenie10_20liter(tekst);
+
+            Assert.That(act, Is.EqualTo(result));
+        }
+        [Test]
+        public void Test_czy_zwraca_string()
+        {
+            string tekst = "1234567890";
+            string result = "string instance";
+
+            string act = _wpis.sprawdzCzyString(tekst);
+
+            Assert.That(act, Is.EqualTo(result));
+        }
+        [Test]
+        public void Test_czy_zwraca_int()
+        {
+            string tekst = "1234567890";
+            string result = "int instance";
+
+            string act = _wpis.sprawdzCzyInt(tekst);
+
+            Assert.That(act, Is.EqualTo(result));
+        }
+    }
+}
